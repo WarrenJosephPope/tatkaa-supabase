35df3f2794e48af88ad61ddea6633eb2f8446ce4
Warren <warrenpope2010@gmail.com>
2025-02-16 03:29:59 +0530

    added migration

---
+++ migrations/20250215214322_made_columns_unique.sql
@@ -0,0 +1,153 @@
+CREATE UNIQUE INDEX profiles_email_key ON public.profiles USING btree (email);
+
+CREATE UNIQUE INDEX profiles_mobile_key ON public.profiles USING btree (mobile);
+
+CREATE UNIQUE INDEX profiles_user_id_key ON public.profiles USING btree (user_id);
+
+alter table "public"."profiles" add constraint "profiles_email_key" UNIQUE using index "profiles_email_key";
+
+alter table "public"."profiles" add constraint "profiles_mobile_key" UNIQUE using index "profiles_mobile_key";
+
+alter table "public"."profiles" add constraint "profiles_user_id_key" UNIQUE using index "profiles_user_id_key";
+
+grant delete on table "public"."addresses" to "postgres";
+
+grant insert on table "public"."addresses" to "postgres";
+
+grant references on table "public"."addresses" to "postgres";
+
+grant select on table "public"."addresses" to "postgres";
+
+grant trigger on table "public"."addresses" to "postgres";
+
+grant truncate on table "public"."addresses" to "postgres";
+
+grant update on table "public"."addresses" to "postgres";
+
+grant delete on table "public"."cart_items" to "postgres";
+
+grant insert on table "public"."cart_items" to "postgres";
+
+grant references on table "public"."cart_items" to "postgres";
+
+grant select on table "public"."cart_items" to "postgres";
+
+grant trigger on table "public"."cart_items" to "postgres";
+
+grant truncate on table "public"."cart_items" to "postgres";
+
+grant update on table "public"."cart_items" to "postgres";
+
+grant delete on table "public"."categories" to "postgres";
+
+grant insert on table "public"."categories" to "postgres";
+
+grant references on table "public"."categories" to "postgres";
+
+grant select on table "public"."categories" to "postgres";
+
+grant trigger on table "public"."categories" to "postgres";
+
+grant truncate on table "public"."categories" to "postgres";
+
+grant update on table "public"."categories" to "postgres";
+
+grant delete on table "public"."categories_fishes" to "postgres";
+
+grant insert on table "public"."categories_fishes" to "postgres";
+
+grant references on table "public"."categories_fishes" to "postgres";
+
+grant select on table "public"."categories_fishes" to "postgres";
+
+grant trigger on table "public"."categories_fishes" to "postgres";
+
+grant truncate on table "public"."categories_fishes" to "postgres";
+
+grant update on table "public"."categories_fishes" to "postgres";
+
+grant delete on table "public"."fishes" to "postgres";
+
+grant insert on table "public"."fishes" to "postgres";
+
+grant references on table "public"."fishes" to "postgres";
+
+grant select on table "public"."fishes" to "postgres";
+
+grant trigger on table "public"."fishes" to "postgres";
+
+grant truncate on table "public"."fishes" to "postgres";
+
+grant update on table "public"."fishes" to "postgres";
+
+grant delete on table "public"."media" to "postgres";
+
+grant insert on table "public"."media" to "postgres";
+
+grant references on table "public"."media" to "postgres";
+
+grant select on table "public"."media" to "postgres";
+
+grant trigger on table "public"."media" to "postgres";
+
+grant truncate on table "public"."media" to "postgres";
+
+grant update on table "public"."media" to "postgres";
+
+grant delete on table "public"."order_details" to "postgres";
+
+grant insert on table "public"."order_details" to "postgres";
+
+grant references on table "public"."order_details" to "postgres";
+
+grant select on table "public"."order_details" to "postgres";
+
+grant trigger on table "public"."order_details" to "postgres";
+
+grant truncate on table "public"."order_details" to "postgres";
+
+grant update on table "public"."order_details" to "postgres";
+
+grant delete on table "public"."orders" to "postgres";
+
+grant insert on table "public"."orders" to "postgres";
+
+grant references on table "public"."orders" to "postgres";
+
+grant select on table "public"."orders" to "postgres";
+
+grant trigger on table "public"."orders" to "postgres";
+
+grant truncate on table "public"."orders" to "postgres";
+
+grant update on table "public"."orders" to "postgres";
+
+grant delete on table "public"."profiles" to "postgres";
+
+grant insert on table "public"."profiles" to "postgres";
+
+grant references on table "public"."profiles" to "postgres";
+
+grant select on table "public"."profiles" to "postgres";
+
+grant trigger on table "public"."profiles" to "postgres";
+
+grant truncate on table "public"."profiles" to "postgres";
+
+grant update on table "public"."profiles" to "postgres";
+
+grant delete on table "public"."reviews" to "postgres";
+
+grant insert on table "public"."reviews" to "postgres";
+
+grant references on table "public"."reviews" to "postgres";
+
+grant select on table "public"."reviews" to "postgres";
+
+grant trigger on table "public"."reviews" to "postgres";
+
+grant truncate on table "public"."reviews" to "postgres";
+
+grant update on table "public"."reviews" to "postgres";
+
+
