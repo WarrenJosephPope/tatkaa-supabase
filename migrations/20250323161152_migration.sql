4fa6dab33b93f7d7a673fe64659e51329e4a240a
Warren <warrenpope2010@gmail.com>
2025-03-23 21:42:54 +0530

    migration

---
+++ migrations/20250323161152_migration.sql
@@ -0,0 +1,61 @@
+alter table "public"."coupons" add column "count" bigint not null default '0'::bigint;
+
+alter table "public"."coupons" add column "description" text;
+
+grant delete on table "public"."approved_pincodes" to "postgres";
+
+grant insert on table "public"."approved_pincodes" to "postgres";
+
+grant references on table "public"."approved_pincodes" to "postgres";
+
+grant select on table "public"."approved_pincodes" to "postgres";
+
+grant trigger on table "public"."approved_pincodes" to "postgres";
+
+grant truncate on table "public"."approved_pincodes" to "postgres";
+
+grant update on table "public"."approved_pincodes" to "postgres";
+
+grant delete on table "public"."homepage_banner" to "postgres";
+
+grant insert on table "public"."homepage_banner" to "postgres";
+
+grant references on table "public"."homepage_banner" to "postgres";
+
+grant select on table "public"."homepage_banner" to "postgres";
+
+grant trigger on table "public"."homepage_banner" to "postgres";
+
+grant truncate on table "public"."homepage_banner" to "postgres";
+
+grant update on table "public"."homepage_banner" to "postgres";
+
+grant delete on table "public"."page_seo" to "postgres";
+
+grant insert on table "public"."page_seo" to "postgres";
+
+grant references on table "public"."page_seo" to "postgres";
+
+grant select on table "public"."page_seo" to "postgres";
+
+grant trigger on table "public"."page_seo" to "postgres";
+
+grant truncate on table "public"."page_seo" to "postgres";
+
+grant update on table "public"."page_seo" to "postgres";
+
+grant delete on table "public"."settings" to "postgres";
+
+grant insert on table "public"."settings" to "postgres";
+
+grant references on table "public"."settings" to "postgres";
+
+grant select on table "public"."settings" to "postgres";
+
+grant trigger on table "public"."settings" to "postgres";
+
+grant truncate on table "public"."settings" to "postgres";
+
+grant update on table "public"."settings" to "postgres";
+
+
